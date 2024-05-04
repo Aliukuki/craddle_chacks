09cd2e1962ad0db702cd8e75822c7b1c9f9e6c51
Aliukuki <35102695+Aliukuki@users.noreply.github.com>
2024-05-04 18:16:15 +0100

    changes

--- main.js
+++ main.js
@@ -86,9 +86,9 @@ function init() {
   controls.enableDamping = true;
   controls.dynamicDampingFactor = 0.01;
   controls.enablePan = false;
-controls.enableZoom = false; // Disable zoom functionality
+  controls.enableZoom = false; // Disable zoom functionality
   controls.minDistance = 200;
-  controls.maxDistance = 500;
+  controls.maxDistance = 200;
   controls.rotateSpeed = 0.8;
   controls.zoomSpeed = 1;
   controls.autoRotate = false;
