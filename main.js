0811791aecae620c4a981ac675b637ca0aad29ed
Aliukuki <35102695+Aliukuki@users.noreply.github.com>
2024-05-04 23:18:54 +0100

    changes

--- main.js
+++ main.js
@@ -148,11 +148,11 @@ function initGlobe() {
   const globeMaterial = Globe.globeMaterial();
   globeMaterial.color = new Color("#fff");
   globeMaterial.emissive = new Color("#fff");
-  globeMaterial.emissiveIntensity = 0.9;
-  globeMaterial.shininess = 0.9;
+  globeMaterial.emissiveIntensity = 0.8;
+  globeMaterial.shininess = 0.8;
   globeMaterial.envMap = null; // Disable the environment map
   globeMaterial.transparent = true;
-globeMaterial.opacity = 0.8; // Adjust the opacity value as needed for a faded look
+globeMaterial.opacity = 0.5; // Adjust the opacity value as needed for a faded look
 
   // NOTE Cool stuff
   // globeMaterial.wireframe = true;
