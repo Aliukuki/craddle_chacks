5630f9f557b728b78d3952db80543fcb36be38d6
Aliukuki <35102695+Aliukuki@users.noreply.github.com>
2024-05-08 14:16:45 +0100

    changes

--- main.js
+++ main.js
@@ -111,8 +111,10 @@ function initGlobe() {
     .atmosphereAltitude(0.1)
     .hexPolygonColor((e) => {
       if (e.properties.ISO_A3 === "NGA") {
-        return colorArray[0];
-      } else return "#E0E0E0";
+        return "green"; // Change color to green for Nigeria
+      } else {
+        return "#E0E0E0"; // Returns a default color for other countries
+      }
     });
   // NOTE Arc animations are followed after the globe enters the scene
   setTimeout(() => {
