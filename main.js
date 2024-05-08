2d94d68e5d6458581c6892f8582c98c6200f23a1
Aliukuki <35102695+Aliukuki@users.noreply.github.com>
2024-05-08 14:14:05 +0100

    changes

--- main.js
+++ main.js
@@ -111,7 +111,7 @@ function initGlobe() {
     .atmosphereAltitude(0.1)
     .hexPolygonColor((e) => {
       if (e.properties.ISO_A3 === "NGA") {
-        return colorArray[0];
+        return green;
       } else return "#E0E0E0";
     });
   // NOTE Arc animations are followed after the globe enters the scene
