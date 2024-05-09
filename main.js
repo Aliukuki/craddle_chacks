c6cf539e03b6ec423ef7bea1c7feaef243786178
Aliukuki <35102695+Aliukuki@users.noreply.github.com>
2024-05-09 09:59:37 +0100

    changes

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ function initGlobe() {
     .atmosphereAltitude(0.1)
     .hexPolygonColor((e) => {
       if (e.properties.ISO_A3 === "NGA") {
-        return "#05DD27"; // Change color to green for Nigeria
+        return "#000"; // Change color to black for Nigeria
       } else {
         return "#E0E0E0"; // Returns a default color for other countries
       }
