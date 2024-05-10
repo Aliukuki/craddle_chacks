fa8a778d6a0b6169f1db209ada6dc620c4899eeb
Aliukuki <35102695+Aliukuki@users.noreply.github.com>
2024-05-10 21:45:24 +0100

    changes

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function init() {
 
   // Initialize scene, light
   scene = new Scene();
-  scene.add(new AmbientLight("#fff", 0));
+  scene.add(new AmbientLight("#F9FFFB", 0.6));
   scene.background = new Color("#F9FFFB");
 
   // Initialize camera, light
